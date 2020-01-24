9a5696e1f1360ae9a738634451fadf2d5be76b5f
Priscila Netto <priscila.nnetto@hotmail.com>
2020-01-23 21:18:23 -0300

    Query Params/Route Params

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express  = require('express');
 
 const server = express();
 
-server.get('/curso', (req, res) => {
- 
-  return res.json({curso: 'Node JS'})
+// Query Params = parametros que passamos na frente da rota Ex: ?nome=NodeJS
+// Route Params = /curso/2
+// Request Body = {nome: 'nodejs', tipo: 'backend'}
+
+server.get('/curso/:id', (req, res) => {
+
+  const id = req.params.id;
+    return res.json ({curso: `Id do curso: ${id}`})
+  
+  //Exemplo abaixo de Query Params
+  // const nome = req.query.nome;
+  // return res.json({curso: `Aprendendo ${nome}`})
   
 })
 server.listen(3001);
