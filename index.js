ef312158bea337ba76884d2c29ab9de637bb8c4c
Priscila Netto <priscila.nnetto@hotmail.com>
2020-01-28 17:22:25 -0300

    exemplo de routes params usando array/index

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const express  = require('express');
 
 const server = express();
 
-// Query Params = parametros que passamos na frente da rota Ex: ?nome=NodeJS
+// Query Params = parametros que passamos na frente da rota 
+// Ex: ?nome=NodeJS
 // Route Params = /curso/2
 // Request Body = {nome: 'nodejs', tipo: 'backend'}
 
-server.get('/curso/:id', (req, res) => {
 
+
+/*server.get('/curso/:id', (req, res) => {
   const id = req.params.id;
-    return res.json ({curso: `Id do curso: ${id}`})
-  
-  //Exemplo abaixo de Query Params
+  return res.json({`Curso: ${id}`});
+} */
+
+//Exemplo abaixo de Query Params
   // const nome = req.query.nome;
-  // return res.json({curso: `Aprendendo ${nome}`})
+  // return res.json({curso: `Aprendendo ${nome}`});
+
+const cursos = ['Node JS', 'JavaScript', 'React Native'];
+
+
+server.get('/curso/:index', (req, res) => {
+
+  const {index} = req.params;
+    return res.json (cursos[index])
+
   
 })
 server.listen(3001);
