c70fd892729a35fbfec59a1e67ee21a73ec8f6cc
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-05 15:49:08 -0300

    criando rotas (crud)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express  = require('express');
 
 const server = express();
 
+server.use(express.json())
+
 // Query Params = parametros que passamos na frente da rota 
 // Ex: ?nome=NodeJS
 // Route Params = /curso/2
@@ -20,13 +22,45 @@ const server = express();
 
 const cursos = ['Node JS', 'JavaScript', 'React Native'];
 
+server.get('/cursos', (req, res) => {
+  return res.json(cursos)
+})
 
-server.get('/curso/:index', (req, res) => {
+
+
+server.get('/cursos/:index', (req, res) => {
 
   const {index} = req.params;
     return res.json (cursos[index])
 
   
 })
-server.listen(3001);
+
+server.post('/cursos', (req, res) => {
+  const {name} = req.body
+  cursos.push(name)
+
+  return res.json(cursos)
+
+})
+ 
+server.put('/cursos/:index', (req, res) => {
+  const {index} = req.params
+    const{name} = req.body
+
+    cursos[index] = name
+ 
+    return res.json(cursos)
+
+
+})
+
+server.delete('cursos/:index', (req, res) => {
+  const {index} = req.params
+  
+    return res.json(cursos)
+})
+
+
+server.listen(3001)
 
