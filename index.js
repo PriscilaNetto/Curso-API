cb79d42f9e8c7883a581eea6e9e1677d6f32d479
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-05 17:59:31 -0300

    criação de middlewares

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express  = require('express');
 
 const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
 // Query Params = parametros que passamos na frente da rota 
 // Ex: ?nome=NodeJS
@@ -22,45 +22,76 @@ server.use(express.json())
 
 const cursos = ['Node JS', 'JavaScript', 'React Native'];
 
+
+//exemplo de um middleware global e usando o next() ele continua, não trava sem colocar nenhuma rota.
+server.use((req, res, next) => {
+
+  console.log(`URL chamada ${req.url}`);
+
+  return next();
+  
+});
+
+
+//usado para verificar se tem o nome do curso. Middleware
+function checkCurso (req, res, next){
+  if(!req.body.name){
+    return res.send("Nome do curso inválido!")
+  }
+  return next()
+}
+
+function checkIndexCurso(req, res, next) {
+  const curso = cursos[req.params.index]
+  
+  if(!curso){
+    return res.send("Id inválido!");
+  }
+
+  req.curso = curso; 
+
+  return next();
+}
+
 server.get('/cursos', (req, res) => {
   return res.json(cursos)
-})
+});
+
+ // exemplo rota usando middleware
+server.get('/cursos/:index', checkIndexCurso, (req, res) => {
+
+    return res.json (req.curso); //não preciso colocar params uma vez que declarei a const(req.curso) na function.
+  
+});
 
 
+server.post('/cursos', checkCurso, (req, res) => {
+  const {name} = req.body;
+  cursos.push(name);
 
-server.get('/cursos/:index', (req, res) => {
+  return res.json(cursos);
 
+});
+ 
+server.put('/cursos/:index', checkCurso, checkIndexCurso, (req, res) => {
   const {index} = req.params;
-    return res.json (cursos[index])
+    const{name} = req.body;
 
-  
-})
-
-server.post('/cursos', (req, res) => {
-  const {name} = req.body
-  cursos.push(name)
-
-  return res.json(cursos)
-
-})
+    cursos[index] = name;
  
-server.put('/cursos/:index', (req, res) => {
-  const {index} = req.params
-    const{name} = req.body
-
-    cursos[index] = name
- 
-    return res.json(cursos)
+    return res.json(cursos);
 
 
-})
+});
 
-server.delete('cursos/:index', (req, res) => {
-  const {index} = req.params
+server.delete('/cursos/:index', checkIndexCurso,  (req, res) => {
+  const {index} = req.params;
+
+  cursos.splice(index, 1); //remove um elemento do índice que será passado no params. 
   
-    return res.json(cursos)
-})
+    return res.send("Curso excluído com sucesso!");
+});
 
 
-server.listen(3001)
+server.listen(3001);
 
